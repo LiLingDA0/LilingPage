1ccaadd679d88a6ed4cef249b1c3baa1092ad06e
LiLingDA0 <lilingda0@gmail.com>
2025-04-09 21:53:24 +0800

    testcommit

--- lilingPage/src/components/HelloWorld.vue
+++ lilingPage/src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      你已经成功created a Vue 3 + Vite project!
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
